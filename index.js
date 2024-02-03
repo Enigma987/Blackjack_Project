6b38c986c7bcf97f463205ccd25a789a43d80d27
Enigma987 <44814640+Enigma987@users.noreply.github.com>
2024-02-03 02:08:49 +0100

    Add more JS

--- index.js
+++ index.js
@@ -32,23 +32,7 @@ function blackjackValues(rank) {
             return 11;
         default:
             return 10;
-    }
-        
-}
-
-function choosePosition(pos) {
-    switch (pos) {
-        case 0:
-            return "zero";
-        case 1:
-            return "one";
-        case 2:
-            return "two";
-        case 3:
-            return "three";
-        case 4:
-            return "four";
-    }
+    }  
 }
 
 function Card(rank, suit) {
@@ -56,8 +40,7 @@ function Card(rank, suit) {
     this.suit = suit;
     this.value = blackjackValues(rank);
     this.name = rank + "_" + suit;
-    var pic_ = new Image();
-    this.pic = pic_.src = "./images/" + this.name + ".png";
+    this.pic = (new Image()).src = "./images/" + this.name + ".png";
 }
 
 function loadAllDeck() {
@@ -68,7 +51,6 @@ function loadAllDeck() {
 
 loadAllDeck();
 leftCardOnDeck = cards;
-console.log(cards);
 
 function randomCard() {
     var randomNumber = Math.floor(Math.random() * leftCardOnDeck.length);
@@ -82,42 +64,62 @@ function randomCard() {
     return card;
 }
 
-function playerTurn() {
+$(".hit").click(function () {
     var playerCard = randomCard();
     playerValue += playerCard.value;
     playerHand.push(playerCard);
 
-    console.log(playerHand[playerHand.length - 1]);
-    console.log(choosePosition(playerHand.length - 1));
-    $("." + choosePosition(playerHand.length-1)).text(playerCard.name);
     $(".playerPoints").text(playerValue);
 
     $('.playerCards').append($("<img class='card' src=" + playerCard.pic + "></p>"));
-}
 
-function croupierTurn() {
-    var croupierCard = randomCard();
-    croupierValue += croupierCard.value;
-    croupierHand.push(croupierCard);
+    if (playerValue === 21) {
+        $("h2").text("Hurra, Blackjack!!!");
 
-    $(".croupierPoints").text(croupierValue);
-}
+        $(".hit").attr("disabled", true);
+        $(".pass").attr("disabled", true);
+    } else if (playerValue > 21) {
+        $("h2").text("Sorry you lose you have above 21 points :(");
 
-
-
-function mainGameLoop() {
-    console.log(leftCardOnDeck);
-    console.log(playerHand + " Value: " + playerValue);
-    console.log(croupierHand + " Value: " + croupierValue);
-
-
-}
-
-$(".hit").click(function () {
-    playerTurn();
-
-    //mainGameLoop();
+        $(".hit").attr("disabled", true);
+        $(".pass").attr("disabled", true);
+    }
 });
+
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function croupierTurn() {
+    var croupierCard;
+    while (croupierValue <= playerValue && croupierValue <= 21) {
+        croupierCard = randomCard();
+
+        croupierValue += croupierCard.value;
+        croupierHand.push(croupierCard);
+
+        $(".croupierPoints").text(croupierValue);
+
+        $('.croupierCards').append($("<img class='card' src=" + croupierCard.pic + "></p>"));
+
+        await this.timeout(Math.random() * 100 + 500);
+    }
+
+    if (croupierValue > playerValue && croupierValue <= 21) {
+        $("h2").text("Sorry you lose - croupier win :(");
+    } else if (croupierValue > 21) {
+        $("h2").text("You win :)");
+    }
+}
+
+
+
+$(".pass").click(function () {
+    $(".hit").attr("disabled", true);
+    $(this).attr("disabled", true);
+
+    croupierTurn();
+})
 
 
 
